30b2f9ef2196e80c5b5c9f62f68b5c45df92ab01
ted_scanlan <t.scanlan92@gmail.com>
2019-11-08 18:35:53 +0000

    can deposit and withdraw and throws exception

--- src/Account.java
+++ src/Account.java
@@ -3,9 +3,28 @@ public class Account {
 	
 	private int balance;
 	
+	public Account() {
+		this(100);
+	}
+	
+	public Account(int balance) {
+		this.balance = balance;
+	}
+	
 	
 	public int getCurrentBalance() {
 		return this.balance;
+	}
+	
+	public void setDepositAmount(int amount) {
+		this.balance += amount;
+	}
+	
+	public void setWithdrawAmount(int amount) {
+		if(this.balance < amount) {
+			throw new IllegalArgumentException("You don't have enough funds!");
+		}
+		this.balance -= amount;
 	}
 	
 
--- src/AccountTest.java
+++ src/AccountTest.java
@@ -5,14 +5,53 @@ import org.junit.Test;
 public class AccountTest {
 
 	@Test
-	public void AccountTest() {
+	public void GetCurrentBalance_Returns0_WhenCalled() {
 		
 		// it begins with an empty balance
-		Account account = new Account();
-		
+		Account account = new Account(0);
 		assertEquals(0, account.getCurrentBalance());
 		
-		
+	
 	}
+	@Test
+	public void setDepositAmount_addToCurrentBalance_WhenCalled() {
+		
+		// it begins with an empty balance
+		Account account = new Account(0);
+		account.setDepositAmount(50);
+		assertEquals(50, account.getCurrentBalance());
+		
+	
+	}
+	
+	@Test
+	public void setWithdrawAmount_subFromCurrentBalance_WhenCalled() {
+		
+		// it begins with an empty balance
+		Account account = new Account(0);
+		account.setDepositAmount(50);
+		account.setWithdrawAmount(25); 
+		assertEquals(25, account.getCurrentBalance());	
+	
+	}
+	
+	
+	@Test(expected = Exception.class)
+	public void whenExceptionThrown_thenExpectationSatisfied() {
+		Account account = new Account(0); 
+		account.setWithdrawAmount(25);	
+	}
+	
+	
+	
+//	public void setWithdrawAmount_raisesErrorIfNoFunds_WhenCalled() {
+//	
+//	
+//	// raises error if users hasnt got enough money
+////		public void test15() throws Exception {
+////			Account account = new Account(0); 	
+////			assertEquals("You don't have enough funds!", account2.setWithdrawAmount(25));
+//
+//	}
 
 }
